d391208cfbbc2c6371e1603cdab53b850fb7dd40
zc <zc15722@163.com>
2022-06-21 22:08:27 +0800

    home-22-6-21

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,34 @@
 import request from "@/utils/request"
 
 //登入
-function login(params={}){
-	return request.post("/users/login",params)
+function login(params = {}) {
+	return request.post("/users/login", params)
 }
 
 //获取菜单项
-function noticeCount(params={}){
-	return request.get("/leave/count",params)
+function noticeCount(params = {}) {
+	return request.get("/leave/count", params)
 }
 
 //获取菜单列表
-function getMenuList(params={}){
-	return request.get("/menu/list",params)
+function getMenuList(params = {}) {
+	return request.get("/menu/list", params)
 }
 
 //获取用户列表
-function getUserList(params={}){
-	return request.get("/users/list",params)
+function getUserList(params = {}) {
+	return request.get("/users/list", params)
+}
+
+//删除用户
+function userDel(params = {}) {
+	return request.post("/users/delete", params)
 }
 
 export default {
 	login,
 	noticeCount,
 	getMenuList,
-	getUserList
+	getUserList,
+	userDel
 }
